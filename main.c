3f674ff652250a7249128e60a7e94e581fe87ffa
Iurynasc <iury0755@hotmail.com>
2018-11-09 19:34:41 -0200

    Update main.c

--- main.c
+++ main.c
@@ -54,13 +54,20 @@ Arvore* carregar_candidatos() {
   //Retornar árvore construída:
   return a;
 }
-
-void buscar_por_numero(Arvore *candidatos) {
-  //Implemente
+void buscar_por_numero(Arvore *candidatos){
+	printf("\nNumero do candidato: ");
+    int numero;
+    scanf("%d", &numero);
+    Candidato *candidato;
+    candidato = arvore_buscar(candidatos, numero);
+    if (candidato != NULL){
+        printf("%d - Nome: %s, Cargo: %s\n", candidato->numero, candidato->nome, candidato->cargo);
+    }
 }
 
 void imprimir_todos(Arvore *candidatos) {
   //Implemente
+  arvore_imprime_em_ordem(candidatos);
 }
 
 int main() {
